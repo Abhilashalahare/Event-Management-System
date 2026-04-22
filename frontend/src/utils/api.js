3a492b178e0c0a869579b55c9a2db8571f837402
Abhilasha Lahare <abhilashalahare6@gmail.com>
2026-04-23 01:14:57 +0530

    Deployement ready

--- frontend/src/utils/api.js
+++ frontend/src/utils/api.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 const api = axios.create({
   // baseURL: 'http://localhost:5001/api', 
-  baseURL: 'https://event-management-system-38gi.onrender.com/api',
+  baseURL: import.meta.env.VITE_API_URL || 'http://localhost:5001/api',
 
 });
 
