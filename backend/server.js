38ccae599d5ee287df15feee9b13b2c8191b5bfd
Abhilasha Lahare <abhilashalahare6@gmail.com>
2026-04-23 01:38:01 +0530

    Deployement Ready

--- backend/server.js
+++ backend/server.js
@@ -10,7 +10,7 @@ const app = express();
 
 
 app.use(cors({
-  origin: import.meta.env.VITE_FRONTEND_URL || 'http://localhost:5173', 
+  origin: '*', 
   credentials: true
 }));
 app.use(express.json());
